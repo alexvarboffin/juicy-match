91f57d5641c0546f4ae4c0c9f5ce7c82e96dff66
r.ivn <ishumakov881@gmail.com>
2025-09-24 20:59:59 +0300

    0

--- uniqulizer/src/main/java/com/walhalla/uniqulizer/M.kt
+++ uniqulizer/src/main/java/com/walhalla/uniqulizer/M.kt
@@ -3,7 +3,26 @@ package com.walhalla.uniqulizer
 class M {
 
 
+//Нужно в классе м создать уникализатор у него много умений
+//    я указываю тип замены
+//
+//    1. тип layout
+//
+//    listOfLayoutsProject
 
+    val project=listOf(
+        "C:\\Users\\combo\\Desktop\\juicy-match\\zsdk",
+
+    )
+
+
+   project + "\src\main\res\layout" - добавить всем xml в папке префикс, или заменить первые части
+    например dialog_  accivity_ на массив соответсвий
+
+    dialog_ to dialog_abc_
+    accivity_ to match_accivity_
+
+    ВАЖНО! рекурсивно обойти код и исправить замены в коде
 
     companion object {
 
